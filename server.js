528cdec91b075e0bc30a423d480b2d8b843436f9
Rudolph Edafe <39104959+tsiyr@users.noreply.github.com>
2022-06-16 15:03:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ const httpServer = createServer()
  // cors: { origin: "*" }
 //});
 
-//const io = new Server (httpServer, {transports: ['websocket']});
+const io = new Server (httpServer, {transports: ['websocket']});
 
-const io = new Server(httpServer);
+//const io = new Server(httpServer);
 
 
 app.use((req, res, next)=>{
@@ -43,7 +43,7 @@ app.use((req, res, next)=>{
 })
 
 // ========== Use Middleware ========== 
-app.use(cors())
+//app.use(cors())
 app.use(bodyParser.json())
 app.use('/user', UserRoute)
 app.use('/cart', CartRoute)
