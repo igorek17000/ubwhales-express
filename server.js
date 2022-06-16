db67cc5422be73ac9718b13a6397aae2177a1a6f
Rudolph Edafe <39104959+tsiyr@users.noreply.github.com>
2022-06-16 15:21:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const binance = new Binance()
 
 // ========== Declare Variable ========== 
 const app = express()
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const DB_URL = process.env.DB_URL
 
 
@@ -36,7 +36,7 @@ app.use((req, res, next)=>{
 })
 
 // ========== Use Middleware ========== 
-//app.use(cors())
+app.use(cors())
 app.use(bodyParser.json())
 app.use('/user', UserRoute)
 app.use('/cart', CartRoute)
@@ -81,7 +81,7 @@ io.on('connection', socket=>{
 
  
  
- const socketServer = httpServer.listen(5555)
+ const socketServer = httpServer.listen(PORT)
 // socketServer.keepAliveTimeout = 61000 * 1000;
 
 const expressServer = app.listen(PORT, ()=>{
